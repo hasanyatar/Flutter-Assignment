2eda8709eeeba41428ad90174c0ef52ddb26179a
Hasan YATAR <hsnytr99@gmail.com>
2023-03-20 01:20:42 +0300

    Fix parameter titles data source

--- app/lib/production/data/remote/titles/data_source/titles_data_source.dart
+++ app/lib/production/data/remote/titles/data_source/titles_data_source.dart
@@ -1,8 +1,7 @@
 import 'package:app/production/data/models/request/titles/titles_request.dart';
-import 'package:app/production/data/models/response/results/results.dart';
 import 'package:app/production/data/models/response/results/title_results.dart';
 
 abstract class TitlesDataSource {
-  Future<List<TitleResults>?> getList({TitlesRequest? filters = const TitlesRequest(page: 1)});
+  Future<List<TitleResults>?> getList({TitlesRequest? filters});
   Future<TitleResults?> getById(String id);
 }
